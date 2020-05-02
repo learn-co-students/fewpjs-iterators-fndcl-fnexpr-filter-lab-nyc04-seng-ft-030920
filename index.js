d8427e622b67540e1a9c942c210af50f256eea1e
GaryZCLau <garyzclau@gmail.com>
2020-05-02 00:52:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+let findMatching = (driver, str) => {
+    return driver.filter(ea => ea.toLowerCase() === str.toLowerCase())
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+  }
